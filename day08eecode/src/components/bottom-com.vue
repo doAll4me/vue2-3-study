8d492978c63d220e95eb9fbc89dbb54216ad2da4
ee <1176187361@qq.com>
2026-01-19 18:53:34 +0800

    '0119-vue3组合式API'

---
+++ day08eecode/src/components/bottom-com.vue
@@ -0,0 +1,19 @@
+<script setup>
+import { inject } from "vue";
+// 普通数据
+const themeColor = inject("theme-color");
+// 响应式数据
+const count = inject("count");
+// 函数
+const changeCount = inject("changeCount");
+const clickFn = () => {
+  changeCount(1000);
+};
+</script>
+
+<template>
+  <div>
+    <h3>我是底层组件-{{ themeColor }} - {{ count }}</h3>
+    <button @click="clickFn">更新count</button>
+  </div>
+</template>
