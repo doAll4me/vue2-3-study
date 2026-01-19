8fa7777221163f8b04a85051dc9c5d36c83fb258
ee <1176187361@qq.com>
2026-01-19 19:01:24 +0800

    '0119-vue'

--- day08eecode/src/App.vue
+++ day08eecode/src/App.vue
@@ -1,27 +1,12 @@
 <script setup>
-import CenterCom from "@/components/center-com.vue";
-import { provide, ref } from "vue";
-
-// 1. 跨层传递普通数据
-provide("theme-color", "pink");
-
-// 2. 跨层传递响应式数据
-const count = ref(100);
-provide("count", count);
-
-setTimeout(() => {
-  count.value = 500;
-}, 2000);
-
-// 3. 跨层传递函数 => 给子孙后代传递可以修改数据的方法
-provide("changeCount", (newCount) => {
-  count.value = newCount;
-});
+import MyInput from "@/components/my-input.vue";
+import { ref } from "vue";
+const txt = ref("123456");
 </script>
 
 <template>
   <div>
-    <h1>我是顶层组件</h1>
-    <CenterCom></CenterCom>
+    <MyInput v-model="txt"></MyInput>
+    {{ txt }}
   </div>
 </template>
--- day08eecode/src/components/my-input.vue
+++ day08eecode/src/components/my-input.vue
@@ -1,17 +1,16 @@
 <script setup>
-import { defineModel } from 'vue'
-const modelValue = defineModel()
+import { defineModel } from "vue";
+//Vue3.3新特性
+// 不用props和emit即可接受和修改父组件的值
+const modelValue = defineModel();
 </script>
 
 <template>
-<div>
-  <input 
-    type="text" 
-    :value="modelValue" 
-    @input="e => modelValue = e.target.value"
-  >
-</div>
+  <div>
+    <input
+      type="text"
+      :value="modelValue"
+      @input="(e) => (modelValue = e.target.value)"
+    />
+  </div>
 </template>
-
-
-
