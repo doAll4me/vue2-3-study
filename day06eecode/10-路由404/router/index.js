fd3e83ef5dd2a374a9d5f085104caf2daa747df8
ee <1176187361@qq.com>
2026-01-28 23:34:22 +0800

    ''

---
+++ day06eecode/10-路由404/router/index.js
@@ -0,0 +1,19 @@
+import MyHome from "@/views/Home";
+import NotFound from "@/views/NotFound.vue";
+import MySearch from "@/views/Search";
+import Vue from "vue";
+import VueRouter from "vue-router";
+Vue.use(VueRouter); // VueRouter插件初始化
+
+// 创建了一个路由对象
+const router = new VueRouter({
+  routes: [
+    { path: "/", redirect: "/home" },
+    { path: "/home", component: MyHome },
+    { path: "/search/:words?", component: MySearch },
+    // 路由404
+    { path: "*", component: NotFound },
+  ],
+});
+
+export default router;
