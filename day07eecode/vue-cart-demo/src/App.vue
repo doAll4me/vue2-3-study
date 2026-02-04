6d283c37c2858803848ccd6b8744b21950bc624e
ee <1176187361@qq.com>
2026-02-04 21:41:25 +0800

    ''

---
+++ day07eecode/vue-cart-demo/src/App.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="app-container">
+    <!-- Header 区域 -->
+    <cart-header></cart-header>
+
+    <!-- 商品 Item 项组件 -->
+    <cart-item v-for="item in list" :key="item.id" :item="item"></cart-item>
+
+    <!-- Foote 区域 -->
+    <cart-footer></cart-footer>
+  </div>
+</template>
+
+<script>
+import CartFooter from "@/components/cart-footer.vue";
+import CartHeader from "@/components/cart-header.vue";
+import CartItem from "@/components/cart-item.vue";
+import { mapActions, mapState } from "vuex";
+
+export default {
+  name: "App",
+  components: {
+    CartHeader,
+    CartFooter,
+    CartItem,
+  },
+  created() {
+    this.getData();
+  },
+  methods: {
+    ...mapActions("cart", ["getData"]),
+  },
+  computed: {
+    ...mapState("cart", ["list"]),
+  },
+};
+</script>
+
+<style lang="less" scoped>
+.app-container {
+  padding: 50px 0;
+  font-size: 14px;
+}
+</style>
