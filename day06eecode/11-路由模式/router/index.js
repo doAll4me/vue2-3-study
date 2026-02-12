141517525dfdc5a4b32c2e64e96d4386b508c166
ee <1176187361@qq.com>
2026-02-13 01:02:34 +0800

    ''

---
+++ day06eecode/11-路由模式/router/index.js
@@ -0,0 +1,23 @@
+import MyHome from "@/views/Home";
+import NotFound from "@/views/NotFound.vue";
+import MySearch from "@/views/Search";
+import Vue from "vue";
+import VueRouter from "vue-router";
+Vue.use(VueRouter); // VueRouter插件初始化
+
+// 创建了一个路由对象
+const router = new VueRouter({
+  routes: [
+    { path: "/", redirect: "/home" },
+    { path: "/home", component: MyHome },
+    { path: "/search/:words?", component: MySearch },
+    // 路由404
+    { path: "*", component: NotFound },
+  ],
+  // 路由模式
+  // hash路由  例如:http://localhost:8080/#/home
+  // history路由（没#）   例如:http://localhost:8080/home
+  mode: "history",
+});
+
+export default router;
